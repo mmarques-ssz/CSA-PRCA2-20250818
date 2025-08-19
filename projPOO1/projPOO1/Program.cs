c51cf4719a2c553f9db912b5a089e349cee71774
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2025-08-18 21:01:03 -0300

    Add files via upload

---
+++ projPOO1/projPOO1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO1
+{
+    internal class Program // CLASSE DA INTERFACE
+    {
+        static void Main(string[] args)
+        {
+            Soma s1;
+            s1 = new Soma();
+
+            #region Entrada
+            Console.WriteLine("Digite o 1º valor: ");
+            s1.setN1(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite o 2º valor: ");
+            s1.setN2(int.Parse(Console.ReadLine()));
+            #endregion
+
+            #region Processo
+            s1.calcular();
+            #endregion
+
+            #region Saída
+            Console.WriteLine("Resultado da soma de {0} e {1} é {2}",
+                              s1.getN1(), s1.getN2(), s1.getResultado());
+            #endregion
+
+        }
+    }
+}
---
+++ projPOO1/projPOO1/Soma.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO1
+{
+    internal class Soma // CLASSE DO NEGÓCIO
+    {
+        #region Atributos
+        private int n1;
+        private int n2;
+        private int resultado;
+        #endregion
+
+        #region Setter's & Getter's
+        public void setN1(int n)
+        {
+            n1 = n;
+        }
+
+        public void setN2(int n)
+        {
+            n2 = n;
+        }
+
+        public int getN1()
+        {
+            return n1;
+        }
+
+        public int getN2()
+        {
+            return n2;
+        }
+
+        public int getResultado()
+        {
+            return resultado;
+        }
+        #endregion
+
+        #region Métodos funcionais
+        public void calcular()
+        {
+            resultado = n1 + n2;
+        }
+        #endregion
+    }
+}
